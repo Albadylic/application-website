3098ff2df2d4674f423913ab79d68d546aa6d697
gregor <gregorlthomson@sky.com>
2020-11-30 18:25:33 +0000

    Wrap interval in function, add button to start carousel

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 // Carousel logic
 
-let carouselImages = Array.from(
+const carouselImages = Array.from(
   document.getElementsByClassName("carousel-image")
 );
 let hiddenImage = 2;
+let myInterval;
 
 const stopButton = document.getElementById("stop-button");
+const startButton = document.getElementById("start-button");
 
 const ticker = () => {
   hiddenImage === 2 ? (hiddenImage = 0) : hiddenImage++;
-  console.log({ hiddenImage });
-  console.log(carouselImages);
   imageDisplayer();
+  imageOrderer();
 };
 
-const myInterval = setInterval(ticker, 3000);
+const startCarousel = () => {
+  myInterval = setInterval(ticker, 3000);
+};
 
 stopButton.addEventListener("click", () => {
   clearInterval(myInterval);
+});
+
+startButton.addEventListener("click", () => {
+  startCarousel();
 });
 
 const imageDisplayer = () => {
@@ -30,4 +37,10 @@ const imageDisplayer = () => {
       item.classList.remove("image-hidden");
     }
   });
+};
+
+const imageOrderer = () => {
+  hiddenImage === 1
+    ? (carouselImages[0].style.order = 1)
+    : (carouselImages[0].style.order = 0);
 };
