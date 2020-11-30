8f6d5d8d3681906cb6e839977ffe4e5b368811d3
gregor <gregorlthomson@sky.com>
2020-11-30 15:27:27 +0000

    Add ticker

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Carousel logic
 
 let carouselImages = document.getElementsByClassName("carousel-image");
-console.log(carouselImages);
+let hiddenImage = 2;
+
+let ticker = () => {
+  hiddenImage === 2 ? (hiddenImage = 0) : hiddenImage++;
+  console.log(hiddenImage);
+};
+
+setInterval(ticker, 3000);
