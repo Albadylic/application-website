a0d949411b47e7309cbc6754406447b9df9a3a18
gregor <gregorlthomson@sky.com>
2020-11-30 16:28:16 +0000

    Add button to stop the ticker

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 let carouselImages = document.getElementsByClassName("carousel-image");
 let hiddenImage = 2;
 
+const stopButton = document.getElementById("stop-button");
+
 let ticker = () => {
   hiddenImage === 2 ? (hiddenImage = 0) : hiddenImage++;
   console.log(hiddenImage);
 };
 
-setInterval(ticker, 3000);
+let myInterval = setInterval(ticker, 3000);
+
+stopButton.addEventListener("click", () => {
+  clearInterval(myInterval);
+});
