21a2e56a764f3046f1044be5b08d17247ff8927c
gregor <gregorlthomson@sky.com>
2020-11-30 17:54:00 +0000

    Adapt logic to cycle images

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 // Carousel logic
 
-let carouselImages = document.getElementsByClassName("carousel-image");
+let carouselImages = Array.from(
+  document.getElementsByClassName("carousel-image")
+);
 let hiddenImage = 2;
 
 const stopButton = document.getElementById("stop-button");
 
-let ticker = () => {
+const ticker = () => {
   hiddenImage === 2 ? (hiddenImage = 0) : hiddenImage++;
-  console.log(hiddenImage);
+  console.log({ hiddenImage });
+  console.log(carouselImages);
+  imageDisplayer();
 };
 
-let myInterval = setInterval(ticker, 3000);
+const myInterval = setInterval(ticker, 3000);
 
 stopButton.addEventListener("click", () => {
   clearInterval(myInterval);
 });
+
+const imageDisplayer = () => {
+  carouselImages.forEach((item, index) => {
+    if (index === hiddenImage) {
+      item.classList.remove("image-visible");
+      item.classList.add("image-hidden");
+    } else {
+      item.classList.add("image-visible");
+      item.classList.remove("image-hidden");
+    }
+  });
+};
